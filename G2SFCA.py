b741572f513c35189a501a3d0e4ab9a4f9b74854
cbnu-uesa <kyusang.kwon@chungbuk.ac.kr>
2024-05-05 14:53:53 +0900

    G2SFCA 코드 수정 및 mapping R코드 추가

--- G2SFCA.py
+++ G2SFCA.py
@@ -8,39 +8,59 @@ def gauss_w (dij, beta):
       weight = np.exp(-dij**2 / beta)
       return weight
 
-
 # generalized 2sfca
-def Generalized2SFCA(data, demand_id, demand_col, supply_id, supply_col, cost_id, catchment, impedance_beta):
+def Generalized2SFCA(data, demand_id, demand_col, supply_id, supply_col, cost_col, catchment, impedance_beta):
     
     print("Procedure started for", demand_id)
     print("\n")
 
     tmp = data.copy()
-    tmp[cost_id] = tmp[cost_id]/60
-    tmp = tmp.loc[tmp[cost_id] <= catchment]
+    tmp[cost_col] = tmp[cost_col] # cost_id는 분(min)으로 측정되어야 함
+    tmp = tmp.loc[tmp[cost_col] <= catchment]
 
-    tmp['weight'] = gauss_w(tmp[[cost_id]], beta = impedance_beta)
+    tmp['weight'] = gauss_w(tmp[[cost_col]], beta = impedance_beta)
     tmp['w_demand'] = tmp[demand_col] * tmp['weight']
     w_demand = tmp.groupby(supply_id, as_index = False)\
         .agg(sum_w_demand = ('w_demand', 'sum'))
-    tmp = tmp.merge(w_demand, how = 'left', on = supply_id)
-
-    if supply_col == 1:
-        tmp['rj'] = 1 / tmp['sum_w_demand']
-        tmp['rj'][np.isinf(tmp['rj'])] = 0   
-    else: 
-        tmp['rj'] = tmp[supply_col] / tmp['sum_w_demand']
-        tmp['rj'][np.isinf(tmp['rj'])] = 0
     
+    rj = tmp[[supply_id, supply_col]].drop_duplicates()
+    rj = rj.merge(w_demand, how = 'left', on = supply_id)
+    rj['rj'] = rj[supply_col] / rj['sum_w_demand']
+    rj['rj'][np.isinf(rj['rj'])] = 0
+    rj = rj[[supply_id, 'rj']]   
+
+    tmp = tmp.merge(rj, how = 'left', on = supply_id)
     tmp['w_rj'] = tmp['rj'] * tmp['weight']
     ai_result = tmp.groupby(demand_id, as_index = False)\
         .agg(accessibility = ('w_rj', 'sum'))
     ai_result[demand_id] = ai_result[demand_id].astype(str)
     ai_result['accessibility'] = ai_result['accessibility']*1000
-    
+
+    avg_access = ai_result['accessibility'].mean()
+    ai_result['spar'] = ai_result['accessibility']/avg_access -1
+
     print('Procedure finished for', demand_id, '\n')
 
     
     return ai_result    
 
+########### 실행코드
+import pandas as pd
+network_data = pd.read_csv('data/network_data.csv')
+demand_data = pd.read_csv('data/demand_df.csv')
+supply_data = pd.read_csv('data/supply_df.csv')
 
+network_data = pd.merge(network_data, demand_data, on='demand_id', how='left')
+network_data = pd.merge(network_data, supply_data, on='supply_id', how='left')
+
+demand_id = 'demand_id'
+demand_col = 'demand_col'
+supply_id = 'supply_id'
+supply_col = 'supply_col'
+cost_col = 'cost_col'
+catchment = 30
+impedance_beta = 200
+
+result = Generalized2SFCA(network_data, demand_id, demand_col, supply_id, supply_col, cost_col, catchment, impedance_beta)
+
+result.to_csv('data/accessibility.csv', index=False, encoding = 'utf-8')
---
+++ mapping.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(sf)
+library(tmap)
+library(extrafont)
+
+
+# Set working directory
+path_wd <- dirname(rstudioapi::getSourceEditorContext()$path)
+setwd(path_wd)
+
+# load fonts: 한번만 실행
+font_import()
+loadfonts()
+
+# load data
+sejong <- read_sf("data/grid_sejong.gpkg")
+access <- read_csv("data/accessibility.csv")
+
+sejong <- left_join(sejong, access, by = c("SPO_NO_CD" = "demand_id"))
+
+## mapping
+tm_shape(sejong) +
+  tm_fill(col = "spar", style = "fixed", n = 6, 
+          breaks = c(-Inf, -0.5, -0.25, 0, 0.25, 0.5, Inf),
+          legend.hist = FALSE, title = "Accessibility (SPAR)",
+          palette = "-RdBu") +
+  tm_layout(legend.outside = FALSE,
+            legend.outside.position = "right",
+            legend.title.size = 1.5,
+            legend.title.fontfamily = "Arial",
+            legend.title.fontface = "bold",
+            legend.text.fontfamily = "Arial",
+            legend.text.size = 1.1) +
+  tm_compass(position = c("left", "bottom"), size = 4, show.labels = 0)+
+  tm_scale_bar(position = c("left", "bottom"), width = 1, size = 1, breaks = c(0, 2.5, 5)) +
+  #tm_shape(seoul_gu)+
+  tm_borders()
