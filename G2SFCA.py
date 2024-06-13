d5e81968779567ebf9672f40e2f71984e4659ba7
cbnu-uesa <kyusang.kwon@chungbuk.ac.kr>
2024-06-13 18:51:20 +0900

    fca 및 mapping 코드 수정

--- G2SFCA.py
+++ G2SFCA.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 import datetime
 
 # gaussian distribution
@@ -37,18 +38,18 @@ def Generalized_2SFCA(demand_data, supply_data, cost_data, threshold):
 
     # 1단계: 공급지별 수요(w_demand)를 고려한 Rj 산정
     data['w_demand'] = data['demand_val'] * data['fij']
-    w_demand = data.groupby(supply_id, as_index = False)\
+    w_demand = data.groupby('supply_id', as_index = False)\
         .agg(sum_w_demand = ('w_demand', 'sum'))
     
-    rj = supply_data.merge(w_demand, how = 'left', on = supply_id)
+    rj = supply_data.merge(w_demand, how = 'left', on = 'supply_id')
     rj['rj'] = rj['supply_val'] / rj['sum_w_demand']
     rj.loc[np.isinf(rj['rj']), ['rj']] = 0
-    rj = rj[[supply_id, 'rj']]   
+    rj = rj[['supply_id', 'rj']]   
 
     # 2단계: 수요지별로 Rj 합산
-    data = data.merge(rj, how = 'left', on = supply_id)
-    data['w_rj'] = data['rj'] * data['weight']
-    access = data.groupby(demand_id, as_index = False)\
+    data = data.merge(rj, how = 'left', on = 'supply_id')
+    data['w_rj'] = data['rj'] * data['fij']
+    access = data.groupby('demand_id', as_index = False)\
         .agg(accessibility = ('w_rj', 'sum'))
     
     avg_access = access['accessibility'].mean()
@@ -62,7 +63,73 @@ def Generalized_2SFCA(demand_data, supply_data, cost_data, threshold):
 
     print(f'Procedure has finished. It takes {duration} seconds')
     
-    return result    
+    return result, data
+
+
+# Adjusted Balanced FCA
+def ABFCA(demand_data, supply_data, cost_data, threshold):
+    
+    start_time = datetime.datetime.now()
+
+    print("Procedure started")
+    print("\n")
+
+    # 컬럼 정리
+    demand_data.columns = ['demand_id', 'demand_val']
+    supply_data.columns = ['supply_id', 'supply_val']
+    cost_data.columns = ['demand_id', 'supply_id', 'cost_val']
+
+    # 분석용 데이터 생성
+    data = cost_data.merge(demand_data, how = 'left', on = 'demand_id')\
+          .merge(supply_data, how = 'left', on = 'supply_id')
+    
+    # threshold에 따른 impedence beta 산정
+    impedance_beta = -(threshold**2)/np.log(0.001)
+    
+    # threshold를 기반으로 불필요한 OD pairs 삭제
+    data = data.loc[data['cost_val'] <= threshold]
+
+    # cost를 distance decay function을 활용하여 weight로 변경
+    data['fij'] = gauss_w(data['cost_val'], beta = impedance_beta) 
+
+    # 0단계: 공급지 매력도를 고려한 수요 배분 확률(Huff 확률) qij 산정
+    data['w_supply'] = data['supply_val'] * data['fij']
+    w_supply = data.groupby('demand_id', as_index = False)\
+        .agg(sum_w_supply = ('w_supply', 'sum'))
+    data = data.merge(w_supply, how = 'left', on = 'demand_id')
+    data['qij'] = data['w_supply'] / data['sum_w_supply']
+
+    # 1단계: 공급지별 수요(w_demand)를 고려한 Rj 산정 
+    #data['w_demand'] = data['demand_val'] * data['qij']
+    data['w_demand'] = data['demand_val'] * data['qij'] * data['fij']
+    w_demand = data.groupby('supply_id', as_index = False)\
+        .agg(sum_w_demand = ('w_demand', 'sum'))
+    
+    rj = supply_data.merge(w_demand, how = 'left', on = 'supply_id')
+    rj['rj'] = rj['supply_val'] / rj['sum_w_demand']
+    rj.loc[np.isinf(rj['rj']), ['rj']] = 0
+    rj = rj[['supply_id', 'rj']]   
+
+    # 2단계: 수요지별로 Rj 합산
+    data = data.merge(rj, how = 'left', on = 'supply_id')
+    #data['w_rj'] = data['rj'] * data['qij']
+    data['w_rj'] = data['rj'] * data['qij'] * data['fij']
+    access = data.groupby('demand_id', as_index = False)\
+        .agg(accessibility = ('w_rj', 'sum'))
+    
+    avg_access = access['accessibility'].mean()
+    access['spar'] = access['accessibility']/avg_access - 1
+
+    result = demand_data.merge(access, how = 'left', on = 'demand_id')
+    result = result[['demand_id', 'accessibility', 'spar']]
+
+    end_time = datetime.datetime.now()
+    duration = end_time - start_time
+
+    print(f'Procedure has finished. It takes {duration} seconds')
+    
+    return result, data
+
 
 # inverted 2sfca
 def inverted_2SFCA(demand_data, supply_data, cost_data, threshold):
@@ -118,68 +185,6 @@ def inverted_2SFCA(demand_data, supply_data, cost_data, threshold):
     print(f'Procedure has finished. It takes {duration} seconds')
     
     return result    
-
-# Adjusted Balanced FCA
-def ABFCA(demand_data, supply_data, cost_data, threshold):
-    
-    start_time = datetime.datetime.now()
-
-    print("Procedure started")
-    print("\n")
-
-    # 컬럼 정리
-    demand_data.columns = ['demand_id', 'demand_val']
-    supply_data.columns = ['supply_id', 'supply_val']
-    cost_data.columns = ['demand_id', 'supply_id', 'cost_val']
-
-    # 분석용 데이터 생성
-    data = cost_data.merge(demand_data, how = 'left', on = 'demand_id')\
-          .merge(supply_data, how = 'left', on = 'supply_id')
-    
-    # threshold에 따른 impedence beta 산정
-    impedance_beta = -(threshold**2)/np.log(0.001)
-    
-    # threshold를 기반으로 불필요한 OD pairs 삭제
-    data = data.loc[data['cost_val'] <= threshold]
-
-    # cost를 distance decay function을 활용하여 weight로 변경
-    data['fij'] = gauss_w(data['cost_val'], beta = impedance_beta) 
-
-    # 0단계: 공급지 매력도를 고려한 수요 배분 확률(Huff 확률) qij 산정
-    data['w_supply'] = data['supply_col'] * data['fij']
-    w_supply = data.groupby(supply_id, as_index = False)\
-        .agg(sum_w_supply = ('w_supply', 'sum'))
-    data = data.merge(w_supply, how = 'left', on = 'supply_id')
-    data['qij'] = data['w_supply'] / data['sum_w_supply']
-
-    # 1단계: 공급지별 수요(w_demand)를 고려한 Rj 산정 
-    data['w_demand'] = data['demand_val'] * data['qij']
-    w_demand = w_demand.groupby(supply_id, as_index = False)\
-        .agg(sum_w_demand = ('w_demand', 'sum'))
-    
-    rj = supply_data.merge(w_demand, how = 'left', on = supply_id)
-    rj['rj'] = rj['supply_val'] / rj['sum_w_demand']
-    rj.loc[np.isinf(rj['rj']), ['rj']] = 0
-    rj = rj[[supply_id, 'rj']]   
-
-    # 2단계: 수요지별로 Rj 합산
-    data = data.merge(rj, how = 'left', on = supply_id)
-    data['w_rj'] = data['rj'] * data['qij']
-    access = data.groupby(demand_id, as_index = False)\
-        .agg(accessibility = ('w_rj', 'sum'))
-    
-    avg_access = access['accessibility'].mean()
-    access['spar'] = access['accessibility']/avg_access - 1
-
-    result = demand_data.merge(access, how = 'left', on = 'demand_id')
-    result = result[['demand_id', 'accessibility', 'spar']]
-
-    end_time = datetime.datetime.now()
-    duration = end_time - start_time
-
-    print(f'Procedure has finished. It takes {duration} seconds')
-    
-    return result 
 
 # Congested Supply Accessibility
 def CongestedFCA(demand_data, supply_data, cost_data, threshold):
@@ -250,22 +255,16 @@ def CongestedFCA(demand_data, supply_data, cost_data, threshold):
     return result 
 
 # Load and prepare data
-network_data = pd.read_csv('c:/p/g2sfca/new_network_data.csv')
-demand_data = pd.read_csv('c:/p/g2sfca/new_demand_data.csv')
-supply_data = pd.read_csv('c:/p/g2sfca/new_supply_data.csv')
+cost_data = pd.read_csv('data/new_network_data.csv')
+demand_data = pd.read_csv('data/new_demand_data.csv')
+supply_data = pd.read_csv('data/new_supply_data.csv')
+threshold = 30
 
-network_data = pd.merge(network_data, demand_data, on='demand_id', how='left')
-network_data = pd.merge(network_data, supply_data, on='supply_id', how='left')
-
-demand_id = 'demand_id'
-demand_col = 'demand_col'
-supply_id = 'supply_id'
-supply_col = 'supply_col'
-cost_col = 'cost_col'
-catchment = 30
-impedance_beta = 130.283
 
 # Execute the modified 2SFCA with ABFC adjustment
-result = Generalized2SFCA_ABFCAdjustment(network_data, demand_id, demand_col, supply_id, supply_col, cost_col, catchment, impedance_beta)
+result_g2sfca, data_g2sfca = Generalized_2SFCA(demand_data, supply_data, cost_data, threshold)
 
-result.to_csv('data/re_accessibility_30.csv', index=False, encoding = 'utf-8-sig')
+result_abfca, data_abfca = ABFCA(demand_data, supply_data, cost_data, threshold)
+
+result_g2sfca.to_csv(f'output/g2sfca_{threshold}.csv', index=False, encoding = 'utf-8-sig')
+result_abfca.to_csv(f'output/abfca_{threshold}.csv', index=False, encoding = 'utf-8-sig')
--- mapping.R
+++ mapping.R
@@ -13,13 +13,31 @@ font_import()
 loadfonts()
 
 # load data
-sejong <- read_sf("data/grid_sejong.gpkg")
-access <- read_csv("data/accessibility.csv")
+sejong <- read_sf("data/sejong_500m_grid.gpkg")
+sejong_supply <- read_sf("data/new_sup_sf.gpkg")
+access <- read_csv("output/g2sfca_30.csv")
 
 sejong <- left_join(sejong, access, by = c("SPO_NO_CD" = "demand_id"))
 
 ## mapping
 tm_shape(sejong) +
+  tm_fill(col = "accessibility", style = "pretty", n = 5, 
+          #breaks = c(-Inf, -0.5, -0.25, 0, 0.25, 0.5, Inf),
+          legend.hist = FALSE, title = "Accessibility",
+          palette = "viridis") +
+  tm_layout(legend.outside = FALSE,
+            legend.outside.position = "right",
+            legend.title.size = 1.5,
+            legend.title.fontfamily = "Arial",
+            legend.title.fontface = "bold",
+            legend.text.fontfamily = "Arial",
+            legend.text.size = 1.1) +
+  tm_compass(position = c("left", "bottom"), size = 4, show.labels = 0)+
+  tm_scale_bar(position = c("left", "bottom"), size = 1, breaks = c(0, 2.5, 5)) +
+  #tm_shape(seoul_gu)+
+  tm_borders()
+
+map <- tm_shape(sejong) +
   tm_fill(col = "spar", style = "fixed", n = 6, 
           breaks = c(-Inf, -0.5, -0.25, 0, 0.25, 0.5, Inf),
           legend.hist = FALSE, title = "Accessibility (SPAR)",
@@ -33,5 +51,9 @@ tm_shape(sejong) +
             legend.text.size = 1.1) +
   tm_compass(position = c("left", "bottom"), size = 4, show.labels = 0)+
   tm_scale_bar(position = c("left", "bottom"), width = 1, size = 1, breaks = c(0, 2.5, 5)) +
-  #tm_shape(seoul_gu)+
   tm_borders()
+
+map <-  map + tm_shape(sejong_supply) +  # sejong_supply 포인트 데이터를 추가합니다
+  tm_dots(size = 0.5, col = "blue", title = "Supply Points")  # 포인트 데이터를 시각화합니다
+
+map
